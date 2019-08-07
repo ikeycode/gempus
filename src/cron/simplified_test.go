5fa342b29732c059b5f15bc1fe1aaeb89078f144
Ikey Doherty <contactikeydoherty@gmail.com>
2019-08-07 01:24:50 +0100

    cron: Add some more testing..
    Signed-off-by: Ikey Doherty <contactikeydoherty@gmail.com>

--- src/cron/simplified_test.go
+++ src/cron/simplified_test.go
@@ -20,6 +20,54 @@ import (
 	"testing"
 )
 
+// Add basic test for simple format
 func TestBasicEvent(t *testing.T) {
-	t.Fatal("Not a proper test suite")
+	event := NewEventSimpleFormatValues(-1, -1, "")
+	if !event.Timing().HasFlag(EventPerHour) {
+		t.Fatal("Missing hour flag")
+	}
+	if !event.Timing().HasFlag(EventPerMinute) {
+		t.Fatal("Missing minute flag")
+	}
+	if !event.Timing().HasFlag(EventRepeats) {
+		t.Fatal("Missing repeat flag")
+	}
+
+	event = NewEventSimpleFormatValues(12, -1, "")
+	if !event.Timing().HasFlag(EventPerMinute) {
+		t.Fatal("Missing minute flag")
+	}
+	if event.Timing().HasFlag(EventPerHour) {
+		t.Fatal("Extra hour flag")
+	}
+}
+
+func TestTextEvent(t *testing.T) {
+	// Good event
+	event, err := NewEventSimpleFormat("* * /bin/bash")
+	if err != nil {
+		t.Fatal("Couldn't parse good string")
+	}
+	if !event.Timing().HasFlag(EventPerHour | EventPerMinute) {
+		t.Fatal("Missing flags on event")
+	}
+
+	event, err = NewEventSimpleFormat("30 * /bin/bash")
+	if err != nil {
+		t.Fatal("Couldn't parse good string")
+	}
+	if !event.Timing().HasFlag(EventPerHour | EventRepeats) {
+		t.Fatal("Missing flags on event")
+	}
+	if event.Timing().HasFlag(EventPerMinute) {
+		t.Fatal("Extra flags on event")
+	}
+	if event.Timing().Minute != 30 {
+		t.Fatal("Invalid minute on event")
+	}
+
+	event, err = NewEventSimpleFormat("s* * /bin/fail")
+	if err == nil {
+		t.Fatal("Shouldn't parse event")
+	}
 }
